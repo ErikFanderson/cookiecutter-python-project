fbb650b04d291ded08825767be5cdbf42157d658
Erik Anderson <efa@eecs.berkeley.edu>
2022-11-18 13:51:55 -0800

    new pypi template

--- {{cookiecutter.project}}/sourceme.sh
+++ {{cookiecutter.project}}/sourceme.sh
@@ -3,18 +3,7 @@
 # Set PYTHONPATH accordingly
 if [ -z "$PYTHONPATH" ]
 then
-    export PYTHONPATH=$PWD
+    export PYTHONPATH=$PWD/src
 else
-    export PYTHONPATH=$PWD:$PYTHONPATH
+    export PYTHONPATH=$PWD/src:$PYTHONPATH
 fi
-
-# Set MYPYPATH accordingly
-if [ -z "$MYPYPATH" ]
-then
-    export MYPYPATH=$PWD/{{cookiecutter.project}}
-else
-    export MYPYPATH=$PWD/{{cookiecutter.project}}:$MYPYPATH
-fi
-
-# Set {{cookiecutter.project.upper()}}_HOME variable
-export {{cookiecutter.project.upper()}}_HOME=$PWD
